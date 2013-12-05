442536a4ae65f78362ed494115c5b70555c5fabe
Ghaida Zahran <ghaida@zurb.com>
2013-12-05 07:10:35 -0800

    Updated foundation to f5

--- sass-foundation/foundation-sass-package/config.rb
+++ sass-foundation/foundation-sass-package/config.rb
@@ -1,13 +1,12 @@
-require 'zurb-foundation'
 # Require any additional compass plugins here.
-
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "stylesheets"
 sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
+images_dir = "images"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
